9dba9d2b9e0b631e0568ae581419b38eaf14b3ce
Yair Marn <133770652+CmonFilo@users.noreply.github.com>
2023-09-21 18:14:41 +0630

    remove download

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Movies from "./pages/movies/Movies";
 import Favorites from "./pages/favorites/Favorites";
-import Downloads from "./pages/downloads/Downloads";
 import Missing from "./pages/missing/Missing";
 import Footer from "./components/Footer";
 import AboutUs from "./pages/aboutUs/AboutUs";
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/downloads" element={<Downloads />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/*" element={<Missing />} />
         <Route path="/:id" element={<MovieDetails/>} />
