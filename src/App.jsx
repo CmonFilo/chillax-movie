b6d724a59bdb6a674adbe82fe96917de8ec42e6a
Yair Marn <133770652+CmonFilo@users.noreply.github.com>
2023-09-21 23:29:17 +0630

    download removed

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import Missing from "./pages/missing/Missing";
 import Footer from "./components/Footer";
 import AboutUs from "./pages/aboutUs/AboutUs";
 import MovieDetails from "./pages/movies/MovieDetails";
-import TVShows from './pages/TVShow/TVShows';
-import TVShow from './pages/TVShow/TVShow';
+import TVShows from "./pages/TVShow/TVShows";
+import TVShow from "./pages/TVShow/TVShow";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/"> 
-        <Route index element={<Movies />} />
-        <Route path="shows">
-        <Route index element={<TVShows />} />
-        <Route path=":id" element={<TVShow />} /></Route>
-        <Route path="favorites" element={<Favorites />} />
-        <Route path="downloads" element={<Downloads />} />
-        <Route path="aboutus" element={<AboutUs />} />
-        <Route path=":id" element={<MovieDetails/>} />
+        <Route path="/">
+          <Route index element={<Movies />} />
+          <Route path="shows">
+            <Route index element={<TVShows />} />
+            <Route path=":id" element={<TVShow />} />
+          </Route>
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="aboutus" element={<AboutUs />} />
+          <Route path=":id" element={<MovieDetails />} />
         </Route>
         <Route path="*" element={<Missing />} />
       </Routes>
