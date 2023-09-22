8aa62d89cdcd2e9d94323cfc293779d24c194bc6
PhyuZinThant1997 <phyuzinthant2008@gmail.com>
2023-09-22 15:00:39 +0630

    details design

--- src/pages/TVShow/TVShow.jsx
+++ src/pages/TVShow/TVShow.jsx
@@ -1,23 +1,24 @@
 // src/components/TVShowDetails.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TVShowDetails = () => {
   const { id } = useParams();
   const [tvShowDetails, setTVShowDetails] = useState(null);
 
   useEffect(() => {
-    const apiKey = '936af801afc98e81ee8d785b94b8e3a8';
+    const apiKey = "936af801afc98e81ee8d785b94b8e3a8";
     const apiUrl = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`;
 
-    axios.get(apiUrl)
-      .then(response => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
         setTVShowDetails(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching TV show details:', error);
+      .catch((error) => {
+        console.error("Error fetching TV show details:", error);
       });
   }, [id]);
 
@@ -27,15 +28,46 @@ const TVShowDetails = () => {
 
   return (
     <div>
-      <h1>{tvShowDetails.name}</h1>
+      <h1
+        style={{ textAlign: "center", margin: "30px 0", fontWeight: "bolder" }}>
+        {tvShowDetails.name}
+      </h1>
       <img
         src={`https://image.tmdb.org/t/p/w500${tvShowDetails.poster_path}`}
         alt={`${tvShowDetails.name} Poster`}
+        style={{
+          width: "300px",
+          height: "480px",
+          marginLeft: "15%",
+          borderRadius: "5%",
+        }}
       />
-      <p>{tvShowDetails.overview}</p>
-      <p>First Air Date: {tvShowDetails.first_air_date}</p>
-      <p>Rating: {tvShowDetails.vote_average}</p>
-      <p>Back To All TV Shows <Link to={'/shows'} style={{textDecoration:'none'}}>"CLICK ME"</Link></p>
+      <div
+        style={{
+          width: "50%",
+          left: "40%",
+          position: "absolute",
+          top: "350px",
+          textAlign: "justify",
+          right: "30px",
+        }}>
+        <p>{tvShowDetails.overview}</p> <br />
+        <br />
+        <p>First Air Date: {tvShowDetails.first_air_date}</p>
+        <p>Rating: {tvShowDetails.vote_average}</p>
+      </div>
+      <p
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          margin: "25px",
+          fontSize: "20px",
+        }}>
+        Back To All TV Shows{" "}
+        <Link to={"/shows"} style={{ textDecoration: "none" }}>
+          "CLICK ME"
+        </Link>
+      </p>
     </div>
   );
 };
--- src/pages/movies/MovieDetails.jsx
+++ src/pages/movies/MovieDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";;
+import { Link, useParams } from "react-router-dom";
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -25,16 +25,43 @@ const MovieDetails = () => {
   }
 
   return (
-    <div>
-      <h1>{movieDetails.title}</h1>
+    <div className="details">
+      <h1
+        style={{ textAlign: "center", margin: "30px 0", fontWeight: "bolder" }}>
+        {movieDetails.title}
+      </h1>
       <img
         src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
         alt={movieDetails.title}
+        style={{
+          width: "300px",
+          height: "480px",
+          marginLeft: "15%",
+          borderRadius: "5%",
+        }}
       />
-      <p>{movieDetails.overview}</p>
-      <p>Release Date: {movieDetails.release_date}</p>
-      <p>Rating: {movieDetails.vote_average}</p>
-      <p>
+      <div
+        className="detailsData"
+        style={{
+          width: "50%",
+          left: "40%",
+          position: "absolute",
+          top: "300px",
+          textAlign: "justify",
+          right: "30px",
+        }}>
+        <div>{movieDetails.overview}</div> <br />
+        <br />
+        <div>Release Date: {movieDetails.release_date}</div>
+        <div>Rating: {movieDetails.vote_average}</div>
+      </div>
+      <p
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          margin: "25px",
+          fontSize: "20px",
+        }}>
         Back To All Movies{" "}
         <Link to={"/"} style={{ textDecoration: "none" }}>
           "CLICK ME"
